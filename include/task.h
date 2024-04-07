be86b158e4bf71489b3fd5c69e4c11d6a685fcd0
FireWallGR <qwert.ting@bk.ru>
2024-04-07 20:51:53 +0300

    first commit

---
+++ include/task.h
@@ -0,0 +1,19 @@
+// Copyright 2024 Gontsov Roman
+#ifndef INCLUDE_TASKS_H_
+#define INCLUDE_TASKS_H_
+
+#include <utility>
+
+double ropeAroundPlanetTask(
+    double planetRadius,
+    double radiusIncrement
+);
+
+double pathAroundPoolAndFencePriceTask(
+    double poolRadius,
+    double pathWidth,
+    double concretePrice,
+    double fencePrice
+);
+
+#endif //INCLUDE_TASKS_H_
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-// Copyright 2022 UNN-CS
+// Copyright 2024 Gontsov Roman
 #include "circle.h"
 
 int main() {
